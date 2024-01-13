54fe753b4eeec8df95d1e4ea4574b427135d98e3
MKS Studio <99986762+monkeys01@users.noreply.github.com>
2024-01-13 05:31:19 +0100

    Add files via upload

---
+++ base_template.sql
@@ -0,0 +1,590 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5deb2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le : lun. 05 sep. 2022 à 20:42
+-- Version du serveur :  10.3.34-MariaDB-0ubuntu0.20.04.1
+-- Version de PHP : 8.0.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `s4_basev1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `addon_account`
+--
+
+CREATE TABLE `addon_account` (
+  `name` varchar(60) NOT NULL,
+  `label` varchar(100) NOT NULL,
+  `shared` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `addon_account`
+--
+
+INSERT INTO `addon_account` (`name`, `label`, `shared`) VALUES
+('property_black_money', 'Argent Sale Propriété', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `addon_account_data`
+--
+
+CREATE TABLE `addon_account_data` (
+  `id` int(11) NOT NULL,
+  `account_name` varchar(100) DEFAULT NULL,
+  `money` int(11) NOT NULL,
+  `owner` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `addon_inventory`
+--
+
+CREATE TABLE `addon_inventory` (
+  `name` varchar(60) NOT NULL,
+  `label` varchar(100) NOT NULL,
+  `shared` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `addon_inventory`
+--
+
+INSERT INTO `addon_inventory` (`name`, `label`, `shared`) VALUES
+('property', 'Propriété', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `addon_inventory_items`
+--
+
+CREATE TABLE `addon_inventory_items` (
+  `id` int(11) NOT NULL,
+  `inventory_name` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `count` int(11) NOT NULL,
+  `owner` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `billing`
+--
+
+CREATE TABLE `billing` (
+  `id` int(11) NOT NULL,
+  `identifier` varchar(40) NOT NULL,
+  `sender` varchar(40) NOT NULL,
+  `target_type` varchar(50) NOT NULL,
+  `target` varchar(40) NOT NULL,
+  `label` varchar(255) NOT NULL,
+  `amount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `characters`
+--
+
+CREATE TABLE `characters` (
+  `id` int(11) NOT NULL,
+  `identifier` varchar(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `dateofbirth` varchar(255) NOT NULL,
+  `sex` varchar(1) NOT NULL DEFAULT 'M',
+  `height` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `datastore`
+--
+
+CREATE TABLE `datastore` (
+  `name` varchar(60) NOT NULL,
+  `label` varchar(100) NOT NULL,
+  `shared` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `datastore`
+--
+
+INSERT INTO `datastore` (`name`, `label`, `shared`) VALUES
+('property', 'Propriété', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `datastore_data`
+--
+
+CREATE TABLE `datastore_data` (
+  `id` int(11) NOT NULL,
+  `name` varchar(60) NOT NULL,
+  `owner` varchar(40) DEFAULT NULL,
+  `data` longtext DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `fine_types`
+--
+
+CREATE TABLE `fine_types` (
+  `id` int(11) NOT NULL,
+  `label` varchar(255) DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL,
+  `category` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `fine_types`
+--
+
+INSERT INTO `fine_types` (`id`, `label`, `amount`, `category`) VALUES
+(1, 'Usage abusif du klaxon', 30, 0),
+(2, 'Franchir une ligne continue', 40, 0),
+(3, 'Circulation à contresens', 250, 0),
+(4, 'Demi-tour non autorisé', 250, 0),
+(5, 'Circulation hors-route', 170, 0),
+(6, 'Non-respect des distances de sécurité', 30, 0),
+(7, 'Arrêt dangereux / interdit', 150, 0),
+(8, 'Stationnement gênant / interdit', 70, 0),
+(9, 'Non respect  de la priorité à droite', 70, 0),
+(10, 'Non-respect à un véhicule prioritaire', 90, 0),
+(11, 'Non-respect d\'un stop', 105, 0),
+(12, 'Non-respect d\'un feu rouge', 130, 0),
+(13, 'Dépassement dangereux', 100, 0),
+(14, 'Véhicule non en état', 100, 0),
+(15, 'Conduite sans permis', 1500, 0),
+(16, 'Délit de fuite', 800, 0),
+(17, 'Excès de vitesse < 5 kmh', 90, 0),
+(18, 'Excès de vitesse 5-15 kmh', 120, 0),
+(19, 'Excès de vitesse 15-30 kmh', 180, 0),
+(20, 'Excès de vitesse > 30 kmh', 300, 0),
+(21, 'Entrave de la circulation', 110, 1),
+(22, 'Dégradation de la voie publique', 90, 1),
+(23, 'Trouble à l\'ordre publique', 90, 1),
+(24, 'Entrave opération de police', 130, 1),
+(25, 'Insulte envers / entre civils', 75, 1),
+(26, 'Outrage à agent de police', 110, 1),
+(27, 'Menace verbale ou intimidation envers civil', 90, 1),
+(28, 'Menace verbale ou intimidation envers policier', 150, 1),
+(29, 'Manifestation illégale', 250, 1),
+(30, 'Tentative de corruption', 1500, 1),
+(31, 'Arme blanche sortie en ville', 120, 2),
+(32, 'Arme léthale sortie en ville', 300, 2),
+(33, 'Port d\'arme non autorisé (défaut de license)', 600, 2),
+(34, 'Port d\'arme illégal', 700, 2),
+(35, 'Pris en flag lockpick', 300, 2),
+(36, 'Vol de voiture', 1800, 2),
+(37, 'Vente de drogue', 1500, 2),
+(38, 'Fabriquation de drogue', 1500, 2),
+(39, 'Possession de drogue', 650, 2),
+(40, 'Prise d\'ôtage civil', 1500, 2),
+(41, 'Prise d\'ôtage agent de l\'état', 2000, 2),
+(42, 'Braquage particulier', 650, 2),
+(43, 'Braquage magasin', 650, 2),
+(44, 'Braquage de banque', 1500, 2),
+(45, 'Tir sur civil', 2000, 3),
+(46, 'Tir sur agent de l\'état', 2500, 3),
+(47, 'Tentative de meurtre sur civil', 3000, 3),
+(48, 'Tentative de meurtre sur agent de l\'état', 5000, 3),
+(49, 'Meurtre sur civil', 10000, 3),
+(50, 'Meurte sur agent de l\'état', 30000, 3),
+(51, 'Meurtre involontaire', 1800, 3),
+(52, 'Escroquerie à l\'entreprise', 2000, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `items`
+--
+
+CREATE TABLE `items` (
+  `name` varchar(50) NOT NULL,
+  `label` varchar(50) NOT NULL,
+  `limit` int(11) NOT NULL DEFAULT -1,
+  `rare` int(11) NOT NULL DEFAULT 0,
+  `can_remove` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `items`
+--
+
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+('bandage', 'Bandage', 20, 0, 1),
+('bread', 'Pain', 10, 0, 1),
+('coffe', 'Café', 10, 0, 1),
+('cupcake', 'Petit Gateau', 10, 0, 1),
+('hamburger', 'Burger', 10, 0, 1),
+('icetea', 'IceTea', 10, 0, 1),
+('medikit', 'Medikit', 5, 0, 1),
+('milk', 'Lait', 10, 0, 1),
+('phone', 'Smarthphone', 1, 0, 1),
+('sandwich', 'Sandwich', 10, 0, 1),
+('water', 'Eau', 10, 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `jobs`
+--
+
+CREATE TABLE `jobs` (
+  `name` varchar(50) NOT NULL,
+  `label` varchar(50) DEFAULT NULL,
+  `SecondaryJob` tinyint(1) NOT NULL DEFAULT 0,
+  `whitelisted` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `jobs`
+--
+
+INSERT INTO `jobs` (`name`, `label`, `SecondaryJob`, `whitelisted`) VALUES
+('unemployed', 'Unemployed', 0, 0),
+('unemployed2', 'Sans emplois', 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `job_grades`
+--
+
+CREATE TABLE `job_grades` (
+  `id` int(11) NOT NULL,
+  `job_name` varchar(50) DEFAULT NULL,
+  `grade` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `label` varchar(50) NOT NULL,
+  `salary` int(11) NOT NULL,
+  `skin_male` longtext NOT NULL,
+  `skin_female` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `job_grades`
+--
+
+INSERT INTO `job_grades` (`id`, `job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
+(2, 'unemployed2', 0, 'rsa', 'Secondaire', 50, '{}', '{}'),
+(26, 'unemployed', 0, 'rsa', 'Secondaire', 50, '{}', '{}');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `licenses`
+--
+
+CREATE TABLE `licenses` (
+  `type` varchar(60) NOT NULL,
+  `label` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `society_moneywash`
+--
+
+CREATE TABLE `society_moneywash` (
+  `id` int(11) NOT NULL,
+  `identifier` varchar(60) NOT NULL,
+  `society` varchar(60) NOT NULL,
+  `amount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `identifier` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `license` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `money` int(11) DEFAULT NULL,
+  `name` varchar(50) COLLATE utf8mb4_bin DEFAULT '',
+  `skin` longtext COLLATE utf8mb4_bin DEFAULT NULL,
+  `job` varchar(50) COLLATE utf8mb4_bin DEFAULT 'unemployed',
+  `job_grade` int(11) DEFAULT 0,
+  `job2` varchar(255) COLLATE utf8mb4_bin DEFAULT 'unemployed2',
+  `job2_grade` int(11) DEFAULT 0,
+  `loadout` longtext COLLATE utf8mb4_bin DEFAULT NULL,
+  `position` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `bank` int(11) DEFAULT NULL,
+  `permission_level` int(11) DEFAULT NULL,
+  `group` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `last_property` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `firstname` varchar(50) COLLATE utf8mb4_bin DEFAULT '',
+  `lastname` varchar(50) COLLATE utf8mb4_bin DEFAULT '',
+  `dateofbirth` varchar(25) COLLATE utf8mb4_bin DEFAULT '',
+  `sex` varchar(10) COLLATE utf8mb4_bin DEFAULT '',
+  `height` varchar(5) COLLATE utf8mb4_bin DEFAULT '',
+  `phone_number` varchar(10) COLLATE utf8mb4_bin DEFAULT NULL,
+  `status` longtext COLLATE utf8mb4_bin DEFAULT NULL,
+  `isDead` bit(1) DEFAULT b'0',
+  `tattoos` longtext COLLATE utf8mb4_bin DEFAULT NULL,
+  `coin` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_accounts`
+--
+
+CREATE TABLE `user_accounts` (
+  `id` int(11) NOT NULL,
+  `identifier` varchar(22) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `money` double NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_inventory`
+--
+
+CREATE TABLE `user_inventory` (
+  `id` int(11) NOT NULL,
+  `identifier` varchar(22) NOT NULL,
+  `item` varchar(50) NOT NULL,
+  `count` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_licenses`
+--
+
+CREATE TABLE `user_licenses` (
+  `id` int(11) NOT NULL,
+  `type` varchar(60) NOT NULL,
+  `owner` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `addon_account`
+--
+ALTER TABLE `addon_account`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Index pour la table `addon_account_data`
+--
+ALTER TABLE `addon_account_data`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `index_addon_account_data_account_name_owner` (`account_name`,`owner`),
+  ADD KEY `index_addon_account_data_account_name` (`account_name`);
+
+--
+-- Index pour la table `addon_inventory`
+--
+ALTER TABLE `addon_inventory`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Index pour la table `addon_inventory_items`
+--
+ALTER TABLE `addon_inventory_items`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `index_addon_inventory_items_inventory_name_name` (`inventory_name`,`name`),
+  ADD KEY `index_addon_inventory_items_inventory_name_name_owner` (`inventory_name`,`name`,`owner`),
+  ADD KEY `index_addon_inventory_inventory_name` (`inventory_name`);
+
+--
+-- Index pour la table `billing`
+--
+ALTER TABLE `billing`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `characters`
+--
+ALTER TABLE `characters`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `datastore`
+--
+ALTER TABLE `datastore`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Index pour la table `datastore_data`
+--
+ALTER TABLE `datastore_data`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `index_datastore_data_name_owner` (`name`,`owner`),
+  ADD KEY `index_datastore_data_name` (`name`);
+
+--
+-- Index pour la table `fine_types`
+--
+ALTER TABLE `fine_types`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Index pour la table `jobs`
+--
+ALTER TABLE `jobs`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Index pour la table `job_grades`
+--
+ALTER TABLE `job_grades`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `licenses`
+--
+ALTER TABLE `licenses`
+  ADD PRIMARY KEY (`type`);
+
+--
+-- Index pour la table `society_moneywash`
+--
+ALTER TABLE `society_moneywash`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`identifier`);
+
+--
+-- Index pour la table `user_accounts`
+--
+ALTER TABLE `user_accounts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `user_inventory`
+--
+ALTER TABLE `user_inventory`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `user_licenses`
+--
+ALTER TABLE `user_licenses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `addon_account_data`
+--
+ALTER TABLE `addon_account_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT pour la table `addon_inventory_items`
+--
+ALTER TABLE `addon_inventory_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `billing`
+--
+ALTER TABLE `billing`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `characters`
+--
+ALTER TABLE `characters`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT pour la table `datastore_data`
+--
+ALTER TABLE `datastore_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT pour la table `fine_types`
+--
+ALTER TABLE `fine_types`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+
+--
+-- AUTO_INCREMENT pour la table `job_grades`
+--
+ALTER TABLE `job_grades`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT pour la table `society_moneywash`
+--
+ALTER TABLE `society_moneywash`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `user_accounts`
+--
+ALTER TABLE `user_accounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT pour la table `user_inventory`
+--
+ALTER TABLE `user_inventory`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
+
+--
+-- AUTO_INCREMENT pour la table `user_licenses`
+--
+ALTER TABLE `user_licenses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
